174761ab4172280977daed15ebf610d0e21d64cf
Manno <manno@xs4all.nl>
2016-01-30 23:38:58 +0100

    Splash Screen Scene added

---
+++ Assets/SplashScreenOverlay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreenOverlay : MonoBehaviour
+{
+	public Texture splashImage;
+
+	LevelManager levelManager;
+
+	void Start()
+	{
+	}
+
+	void OnGUI()
+	{
+		GUI.matrix = Matrix4x4.TRS( new Vector3( 0, 0, 0 ), Quaternion.identity, new Vector3( Screen.width/1600.0f, Screen.height/900.0f, 1.0f ) );
+
+		GUI.DrawTexture( new Rect( 0, 0, 1600, 900 ), splashImage );
+	}
+}
