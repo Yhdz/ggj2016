5e6f8e7ceed817465097854be5f90cbde7e19996
illco <e.p.braad@gmail.com>
2016-01-31 10:13:16 +0100

    Added Level Ctrl / Screenshot script (add to a level to work with F2-F6)

---
+++ Assets/Scripts/LevelSkipResetCheats.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelSkipResetCheats : MonoBehaviour
+{
+	void Update()
+    {
+	    if ( Input.GetKeyDown( KeyCode.F2 ) )
+            SceneManager.LoadScene( (SceneManager.GetActiveScene().buildIndex - 1 + SceneManager.sceneCount ) % SceneManager.sceneCount );
+        if( Input.GetKeyDown( KeyCode.F3 ) )
+            SceneManager.LoadScene( SceneManager.GetActiveScene().buildIndex );
+        if ( Input.GetKeyDown( KeyCode.F4 ) )
+            SceneManager.LoadScene( (SceneManager.GetActiveScene().buildIndex + 1 + SceneManager.sceneCount) % SceneManager.sceneCount );
+        if( Input.GetKeyDown( KeyCode.F5 ) )
+            SceneManager.LoadScene( 0 );
+        if( Input.GetKeyDown( KeyCode.F6 ) )
+            Application.CaptureScreenshot( "screenshot"+Mathf.FloorToInt(Time.timeSinceLevelLoad)+".png" );
+
+    }
+}
