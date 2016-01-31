98ef7388566505045018ea6e71452d0f9e65c566
illco <e.p.braad@gmail.com>
2016-01-31 13:17:58 +0100

    Different Beats possible for Other Dancers

--- Assets/Scripts/DancePattern.cs
+++ Assets/Scripts/DancePattern.cs
@@ -4,17 +4,20 @@ using System.Collections;
 [System.Serializable]
 public class DancePattern : MonoBehaviour
 {
+    public int maxMoves = 3;
+
     public Vector2[] moves;
 
     public AnimationCurve[] curves;
 
 	public Vector2 GetMove(int beat)
     {
-		return moves[beat];
+        Debug.Log( beat + " of " + maxMoves + "/"+moves.Length);
+		return moves[beat%maxMoves];
     }
 
 	public AnimationCurve GetCurve(int beat)
     {
-        return curves[beat];
+        return curves[beat%maxMoves];
     }
 }
